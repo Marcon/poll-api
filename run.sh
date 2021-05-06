de46eef38e84683e6013a1906f00dd1afd027286
Evgeniy Dolgikh <marcon@atsy.su>
2021-05-06 22:34:41 +0300

    create superuser at startup

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
 gunicorn pollapi.wsgi:application --bind 0.0.0.0:8000
